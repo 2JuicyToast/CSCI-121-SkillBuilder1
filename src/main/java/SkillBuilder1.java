e17cd50afc617fc87ee126f4067f3e61524d6de4
Bacon <parkerjoshua0403@gmail.com>
2026-02-06 19:35:15 -0500

    Code added from blueJ, waiting to meet with professor

--- src/main/java/SkillBuilder1.java
+++ src/main/java/SkillBuilder1.java
@@ -9,7 +9,19 @@ import java.util.Scanner;
 public class SkillBuilder1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        //TODO : Replace this comment with your code
+
+        System.out.println("You're Jane's friend!");
+        System.out.println("\"What's your name\"");
+        String name = input.nextLine();
+
+        System.out.println("Enter a floating-point number:");
+        double spice = input.nextDouble();
+
+        spice = (4.0 / 3.0) * ((2 * Math.sqrt(5)) / Math.pow(spice, 3)); //found through online research
+        System.out.println("Well " + name + ", the spice value resulted in " + spice);
+
+        spice = (int)(spice * 100) / 100.0;
+        System.out.println("And the converted value is " + (spice * 100));
     }
 
     public static void calcWallPaint() {
@@ -18,7 +30,7 @@ public class SkillBuilder1 {
         final double squareFeetPerGallons = 350.0;
         final double gallonsPerCan = 1.0;
 
-        // Prompt user to input wall's width
+        // Prompt user to input wall's height
         System.out.println("Enter wall height (feet):");
         double wallHeight = input.nextDouble();
 
@@ -27,9 +39,14 @@ public class SkillBuilder1 {
         double wallWidth = input.nextDouble();
 
         // TODO: Calculate and output the wall's area
+        double area = wallHeight * wallWidth;
 
         // TODO: Calculate and output the amount of paint in gallons needed to paint the wall
+        double paintNeeded = area / squareFeetPerGallons;
+        System.out.printf("Paint needed: %.2f gallons%n", paintNeeded);
 
         // TODO: Calculate and output the number of 1 gallon cans needed to paint the wall, rounded up to nearest integer
+        double cansNeeded = Math.ceil(paintNeeded) * gallonsPerCan; // rounds to nearest whole number, found online
+        System.out.println("Cans needed: " + cansNeeded + " can(s)");
     }
 }
